8b4c95dbf94473db2a8673c446effe5561df8f0b
Max Shaptala <maxshaptala@gmail.com>
2021-03-24 16:39:54 +0200

    Зарегестрировано представление home по пути /home

--- todolist/urls.py
+++ todolist/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp import views
 
 urlpatterns = [
+    path('home/', views.hello),
     path('admin/', admin.site.urls),
 ]
