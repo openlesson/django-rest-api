9b9aef8559258502cbc1a15037b4fe2d49b3d198
Max Shaptala <maxshaptala@gmail.com>
2021-03-24 17:15:06 +0200

    Добавлена модель Task, зарегестрирована в админ панели

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'updated')
+    list_filter = ('priority',)
+
+
+admin.site.register(Task, TaskAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField()
+    choices = (('H', 'HIGH'), ('M', 'MEDIUM'), ('L', 'LOW'))
+    priority = models.CharField(max_length=1, choices=choices)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, default=None, null=True, on_delete=models.CASCADE)
