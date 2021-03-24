0f077f259215f4f9546c6c88c3822c3292f20d31
Max Shaptala <maxshaptala@gmail.com>
2021-03-24 16:39:27 +0200

    Добавлено представление home

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def hello(request):
+    print('Method', request.method)
+    print('Headers', request.headers)
+    print('Body', request.body)
+    return HttpResponse('<h1>Hello Django World!</h1><p>It is very easy!</p>')
