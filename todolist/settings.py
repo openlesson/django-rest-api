839b0e08cb61ce55d6aa96cb0090b5b20d1e4494
Max Shaptala <maxshaptala@gmail.com>
2021-03-25 16:24:47 +0200

    Добавлено новое приложение для api

--- todolist/settings.py
+++ todolist/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp' # устанавливаем наше приложение
+    'webapp', # устанавливаем наше приложение
+    'api'
 ]
 
 MIDDLEWARE = [
