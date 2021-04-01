2ae05c6574e6cc0169aa32bd741dec78657a490a
Max Shaptala <maxshaptala@gmail.com>
2021-04-01 16:38:36 +0300

    Добавили настройки для взаимодействия с REST через JWT

--- todolist/settings.py
+++ todolist/settings.py
@@ -122,5 +122,8 @@ STATICFILES_DIRS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
