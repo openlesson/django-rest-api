98076a872c311154e3a5e9b95638322d45e49132
Max Shaptala <maxshaptala@gmail.com>
2021-04-01 17:00:58 +0300

    Добавлены ограничения для TaskViewSet и TagViewSet: досутп для чтения разрешить всем, на изменения, удаления и создания - только аутентифицированным пользователям

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from .serializers import TaskSerializers, TagSerializers
 from webapp.models import Task, Tag
 
@@ -6,8 +8,10 @@ from webapp.models import Task, Tag
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializers
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializers
+    permission_classes = (IsAuthenticatedOrReadOnly,)
