f151755cc60e5eb6050d8af579be203bab7e5dc7
Max Shaptala <maxshaptala@gmail.com>
2021-04-01 16:42:11 +0300

    Зарегестрированы конечные точки api/token для получения JWT токена и api/token/refresh для обновления access токуна

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,14 @@ from django.urls import path, include
 from rest_framework import routers
 
 from api.views import TaskViewSet, TagViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register('tasks', TaskViewSet)
 router.register('tags', TagViewSet)
 
 urlpatterns = [
-    path('v1/', include(router.urls))
+    path('v1/', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
